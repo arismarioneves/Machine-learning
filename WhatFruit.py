2d8ba807a6f0110433b60988135fba26444455a8
Arismário Neves <arismarioneves@users.noreply.github.com>
2020-04-14 13:19:17 -0300

    Update WhatFruit.py

--- WhatFruit.py
+++ WhatFruit.py
@@ -1,27 +1,24 @@
-#Created by Arismário Neves
-#25/01/2019
-
 from sklearn import tree
 
-lisa = 1
-irregular = 0
+lisa = 0
+irregular = 1
 
-maca = 1
-laranja = 0
+maca = 0
+laranja = 1
 
-arvore = [[150, lisa], [130, lisa], [180, irregular], [160, irregula]]
+arvore = [[130, lisa], [150, lisa], [160, irregular], [180, irregular]]
 
 resultado = [maca, maca, laranja, laranja]
 
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(pomar, resultado)
+clf = clf.fit(arvore, resultado)
 
 peso = int(input("Informe o peso da Fruta: "))
-superficie = int(input("Informe a superficie [Lisa - 1] [Irregular - 0]: "))
+superficie = int(input("Informe a superficie [Lisa - 0] [Irregular - 1]: "))
 
-resultado = cl.predict([[peso, superficie]])
+resultado = clf.predict([[peso, superficie]])
 
-if resultado == 1:
+if resultado == 0:
     print("É uma Maça")
 else:
-    print("É uma Banana")
+    print("É uma Laranja")
